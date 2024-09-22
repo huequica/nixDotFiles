23c31fc0469bd23b09a7df92172358c6c887967d
huequica <dev@huequica.xyz>
2024-09-23 04:07:30 +0900

    change gh git protocol to ssh and insert ssh config from 1password

--- home-manager/cli/git.nix
+++ home-manager/cli/git.nix
@@ -1,14 +1,31 @@
-{ pkgs, pkgs-stable, ...}:
+{ pkgs, lib, pkgs-stable, ...}:
 {
   programs.git = {
     enable = true;
     userName = "huequica";
     userEmail = "dev@huequica.xyz";
+
+    extraConfig = {
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      };
+      commit = {
+        gpgsign = true;
+      };
+
+      user = {
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAzrPOJ5QeoNTLuBc6q6QaX2aZDU7KoHk+vUFhDXMEaT";
+      };
+    };
   };
 
   programs.gh = {
     enable = true;
     package = pkgs-stable.gh;
     settings.editor = "vim";
+    settings.git_protocol = "ssh";
   };
 }

0eaf57203e5a6b6d6b7abdcd9636422a29ceb34c
huequica <dev@huequica.xyz>
2024-09-23 02:42:04 +0900

    fix failed to install 1password

--- home-manager/gui/vault.nix
+++ home-manager/gui/vault.nix
@@ -1,9 +1,7 @@
+{ pkgs, ... }:
 {
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    polkitPolicyOwners = [ 
-      "huequica"
-     ];
-  };
+  home.packages = with pkgs; [
+    _1password
+    _1password-gui
+  ];
 }

d9a1b72b6f6529c9e110107370132418ce98dc7f
huequica <dev@huequica.xyz>
2024-09-23 03:20:44 +0900

    replace vanilla vivaldi to customVivaldi

--- home-manager/gui/browser.nix
+++ home-manager/gui/browser.nix
@@ -1,7 +1,22 @@
+{ pkgs, ...}:
+let 
+  customVivaldi = pkgs.vivaldi.overrideAttrs(
+    oldAttrs: {
+      dontWrapQtApps = false;
+      dontPatchELF = false;
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+    }
+  );
+in
 {
   programs = {
     firefox.enable = true;
-    vivaldi.enable = true;
     google-chrome.enable = true;
   };
+
+  # plasma6 では現状 vivaldi が正しく起動しない問題がある
+  # 
+  home.packages = [
+    customVivaldi
+  ];
 }

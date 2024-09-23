17dccdfe22d6fce9f30f3536729a0b2292b3b339
huequica <dev@huequica.xyz>
2024-09-24 03:54:39 +0900

    auto format from treefmt

--- home-manager/gui/messaging/default.nix
+++ home-manager/gui/messaging/default.nix
@@ -1,10 +1,10 @@
-{pkgs,...}:
+{ pkgs, ... }:
 {
   home.file = {
     ".config/discord/settings.json".text = builtins.readFile ./settings.json;
   };
 
-  home.packages = with pkgs;[
+  home.packages = with pkgs; [
     slack
     discord
   ];

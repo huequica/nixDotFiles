6244570dd2c3eb857046e5c09a62be2e0e3961ee
huequica <dev@huequica.xyz>
2024-09-24 20:24:37 +0900

    add KDEConnect enabling script

--- modules/desktop/plasma6.nix
+++ modules/desktop/plasma6.nix
@@ -3,6 +3,9 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  # smartphone integration
+  programs.kdeconnect.enable = true;
+
   services.xserver.xkb = {
     layout = "us";
     variant = "";

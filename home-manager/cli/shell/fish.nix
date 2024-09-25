2595bfc57b4bdd863ab75a85ee16a05eef9bdbed
huequica <dev@huequica.xyz>
2024-09-26 03:28:38 +0900

    move volta path setup code from `~/.config/fish/config.fish` to fish.nix

--- home-manager/cli/shell/fish.nix
+++ home-manager/cli/shell/fish.nix
@@ -6,5 +6,11 @@
       gdel = "git branch -D";
       nd = "nix develop";
     };
+
+    loginShellInit = ''
+      # volta setup code
+      set -gx VOLTA_HOME "$HOME/.volta"
+      set -gx PATH "$VOLTA_HOME/bin" $PATH
+    '';
   };
 }

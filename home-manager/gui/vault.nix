60e187ef5eb448cd797dd1315ca11a6257b2b438
huequica <dev@huequica.xyz>
2024-09-26 02:09:57 +0900

    add polkit policy owners specify

--- home-manager/gui/vault.nix
+++ home-manager/gui/vault.nix
@@ -1,5 +1,17 @@
-{ pkgs, ... }:
 {
+  nixpkgs,
+  pkgs,
+  ...
+}:
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      _1password-gui = prev._1password-gui.override {
+        polkitPolicyOwners = [ "huequica" ];
+      };
+    })
+  ];
+
   home.packages = with pkgs; [
     _1password
     _1password-gui

844573b6841d4f6f851d5361b7bd983a8c423c95
huequica <dev@huequica.xyz>
2024-09-27 02:24:29 +0900

    move vault to modules/desktop layer

---
+++ modules/desktop/vault.nix
@@ -0,0 +1,10 @@
+{ username, ... }:
+{
+  nixpkgs.config.allowUnfree = true;
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "${username}" ];
+  };
+}

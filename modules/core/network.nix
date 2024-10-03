b1553d75e2cb9b7f7b125a01af2ea713a9ac51c8
huequica <dev@huequica.xyz>
2024-10-04 03:13:32 +0900

    add expressvpn support

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,7 +1,17 @@
-{ config, hostname, ... }:
+{
+  config,
+  hostname,
+  pkgs,
+  ...
+}:
 {
   networking = {
     hostName = hostname;
     networkmanager.enable = true;
   };
+
+  services.expressvpn.enable = true;
+  environment.systemPackages = with pkgs; [
+    expressvpn
+  ];
 }

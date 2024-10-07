ecb71479c05682870b04d1c6c881977ab1a17cd3
huequica <dev@huequica.xyz>
2024-10-07 18:27:12 +0900

    fix cannot push git commits via jetbrains ide problem

--- home-manager/cli/ssh.nix
+++ home-manager/cli/ssh.nix
@@ -10,4 +10,10 @@ in
           IdentityAgent ${onePassPath}
     '';
   };
+
+  # to fix cannot execute `git push` via jetbrains IDE problem
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+  };
 }

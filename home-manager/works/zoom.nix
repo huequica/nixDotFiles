00fb89e3942fcdc6b56ce8c63c0678d1ff6a2333
huequica <dev@huequica.xyz>
2025-01-08 11:03:33 +0900

    downgrade zoom-us package

--- home-manager/works/zoom.nix
+++ home-manager/works/zoom.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    zoom-us
+    # latest zoom-us has problem that failed to launch
+    # https://github.com/NixOS/nixpkgs/issues/371488#issuecomment-2575970402
+    (zoom-us.overrideAttrs {
+      version = "6.2.11.5069";
+      src = pkgs.fetchurl {
+        url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+        hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+      };
+    })
   ];
 }

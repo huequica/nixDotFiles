8dd0e558fd6a74bf1496851c098426a00543972f
huequica <dev@huequica.xyz>
2025-04-30 17:00:12 +0900

    remove outdated zoom-us version specify

--- home-manager/works/zoom.nix
+++ home-manager/works/zoom.nix
@@ -1,14 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # latest zoom-us has problem that failed to launch
-    # https://github.com/NixOS/nixpkgs/issues/371488#issuecomment-2575970402
-    (zoom-us.overrideAttrs {
-      version = "6.2.11.5069";
-      src = pkgs.fetchurl {
-        url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
-        hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
-      };
-    })
+    zoom-us
   ];
 }

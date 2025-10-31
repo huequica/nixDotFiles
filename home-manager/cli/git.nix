fec719d308f075620392cfbf2577fe96e628c3c6
huequica <dev@huequica.xyz>
2025-11-01 08:11:18 +0900

    replace git extraConfig to `settings` object specify

--- home-manager/cli/git.nix
+++ home-manager/cli/git.nix
@@ -8,26 +8,20 @@
   programs.git = {
     enable = true;
     settings = {
-      userName = "huequica";
-      userEmail = "dev@huequica.xyz";
-    };
+      user = {
+        name = "huequica";
+        email = "dev@huequica.xyz";
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAzrPOJ5QeoNTLuBc6q6QaX2aZDU7KoHk+vUFhDXMEaT";
+      };
 
-    extraConfig = {
       core.editor = "vim";
 
-      gpg = {
-        format = "ssh";
-      };
+      gpg.format = "ssh";
+
       "gpg \"ssh\"" = {
         program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
       };
-      commit = {
-        gpgsign = true;
-      };
-
-      user = {
-        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAzrPOJ5QeoNTLuBc6q6QaX2aZDU7KoHk+vUFhDXMEaT";
-      };
+      commit.gpgsign = true;
     };
   };
 

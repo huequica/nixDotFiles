9fd6aee8634e021528dfc6744c50e9f5d61b3a0f
huequica <dev@huequica.xyz>
2025-12-27 23:58:04 +0900

    remove custom login theme

--- modules/desktop/plasma6.nix
+++ modules/desktop/plasma6.nix
@@ -1,10 +1,4 @@
 { pkgs, ... }:
-let
-  sddmWallpaper = pkgs.fetchurl {
-    url = "https://supersonico.jp/download/wallpaper/57_birthday2022_1920x1200.jpg";
-    sha256 = "sha256-6/EOoy1ekwK1014NWLacp654JpvaHsgxQZmwTn7sWLE=";
-  };
-in
 {
   services.xserver.enable = true;
   security.pam.services.kwallet = {
@@ -20,18 +14,8 @@ in
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # custom login theme
-  environment.systemPackages = [
-    (pkgs.catppuccin-sddm.override {
-      flavor = "mocha";
-      background = "${sddmWallpaper}";
-      loginBackground = true;
-    })
-  ];
-
   services.displayManager.sddm = {
     enable = true;
-    theme = "catppuccin-mocha";
     wayland.enable = true;
   };
 }

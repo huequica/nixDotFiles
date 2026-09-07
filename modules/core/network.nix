939e52556dd8c6370accc0afdd37044a29c0256d
huequica <dev@huequica.xyz>
2026-09-07 19:42:56 +0900

    remove expressvpn module

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,21 +1,7 @@
-{
-  config,
-  hostname,
-  pkgs,
-  ...
-}:
+{ hostname, ... }:
 {
   networking = {
     hostName = hostname;
     networkmanager.enable = true;
-    # expressvpn がうまく使用できないことがあるので openvpn も入れておいてマニュアルで認証する
-    networkmanager.plugins = [ pkgs.networkmanager-openvpn ];
-
-    firewall.enable = false;
   };
-
-  services.expressvpn.enable = true;
-  environment.systemPackages = with pkgs; [
-    expressvpn
-  ];
 }

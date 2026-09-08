9dcff4ac040ea8cf55463798aa3d2aa7fc427484
huequica <dev@huequica.xyz>
2026-09-08 10:42:46 +0900

    fix: なんか darwin は証明書の関係で自前ビルドするとつらいことが多いので消す

--- home-manager/gui/mousehop.nix
+++ home-manager/gui/mousehop.nix
@@ -1,3 +1,6 @@
+{ pkgs, ... }:
 {
-  programs.mousehop.enable = true;
+  # NOTE: darwin の場合は証明書の関係で自前で dmg をインストールする必要あり
+  # 現状 brew にも cask として登録されてなさそうなので暫定処置
+  programs.mousehop.enable = pkgs.stdenv.hostPlatform.isLinux;
 }
